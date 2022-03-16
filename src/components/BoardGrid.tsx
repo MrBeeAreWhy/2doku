888a6e8d53cce08ae1360f2875123487be472885
Bryan Bart <bebart87@gmail.com>
2022-03-16 14:27:36 -0700

    board has working timer via redux, the board will validate the answer is correct or not, stops timer and displays 'win' condition when game is won

--- src/components/BoardGrid.tsx
+++ src/components/BoardGrid.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import BoardBox from './BoardBox';
 import StartGame from './StartGame';
 import { useAppSelector } from '../app/hooks';
@@ -7,21 +7,25 @@ function BoardGrid() {
   const initialBoard = useAppSelector((state) => state.board.board);
   const activePlays = useAppSelector((state) => state.board.activePlays);
   const gameStarted = useAppSelector((state) => state.board.gameStarted);
+  const gameWon = useAppSelector((state) => state.board.gameWon);
   const displayBoxes = [];
+  const handleClick = (event: MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  }
   
-  if (gameStarted){
+  if (gameStarted || gameWon){
     for (let i = 0; i < initialBoard.length; i++){
       for (let j = 0; j < initialBoard[i].length; j++){
         if (initialBoard[i][j] === 0){
             if (activePlays[i][j] !== 0){
-              displayBoxes.push(<BoardBox preset={false} displayValue={`${activePlays[i][j]}`} location={[i,j]}/>)
+              displayBoxes.push(<BoardBox preset={false} key={`x${[i]}y${[j]}`} displayValue={`${activePlays[i][j]}`} location={[i,j]} gameWon={gameWon}/>)
             } else {
-              displayBoxes.push(<BoardBox preset={false} displayValue={``} location={[i,j]}/>)
+              displayBoxes.push(<BoardBox preset={false} key={`x${[i]}y${[j]}`} displayValue={``} location={[i,j]} gameWon={gameWon}/>)
             }
         } else if (initialBoard[i][j] === 9){
 
         } else {
-          displayBoxes.push(<BoardBox preset={true} displayValue={`${initialBoard[i][j]}`} location={[i,j]}/>)
+          displayBoxes.push(<BoardBox preset={true} key={`x${[i]}y${[j]}`} displayValue={`${initialBoard[i][j]}`} location={[i,j]} gameWon={gameWon}/>)
         }
       }  
     }
@@ -29,8 +33,8 @@ function BoardGrid() {
 
   return (
     <div>
-      {gameStarted ? 
-      <div>
+      {gameStarted === true || (gameStarted === false && gameWon === true) ? 
+      <div onContextMenu={handleClick}>
         <div className="BoardGrid">
           {displayBoxes}
         </div>
--- src/slices/boardSlice.ts
+++ src/slices/boardSlice.ts
@@ -5,10 +5,10 @@ interface BoardState {
   board: [number[], number[], number[], number[]],
   activePlays: [number[], number[], number[], number[]],
   gameStarted: boolean,
-  activeSection: string,
+  gameWon: boolean,
+  boardFilled: boolean,
   startTime: number,
   currentTime: number,
-  interval: any
 }
 
 interface UpdateAction {
@@ -20,19 +20,19 @@ interface UpdateAction {
 const initialState: BoardState = {
   board: 
           [[0,2,0,0],
-          [0,0,0,0],
-          [0,0,2,0],
-          [0,0,0,3]],
+           [0,0,0,4],
+           [0,0,3,0],
+           [1,0,0,2]],
   activePlays: 
-          [[0,0,0,0],
-          [0,0,0,0],
-          [0,0,0,0],
-          [0,0,0,0]],
+          [[0,2,0,0],
+           [0,0,0,4],
+           [0,0,3,0],
+           [1,0,0,2]],
   gameStarted: false,
+  gameWon: false,
+  boardFilled: false,
   startTime: 0,
   currentTime: 0,
-  activeSection: 'none',
-  interval: null,  
 }
 
 export const boardSlice = createSlice({
@@ -44,22 +44,42 @@ export const boardSlice = createSlice({
       state.startTime = Date.now()
 
     },
-    updateActive: (state, action: PayloadAction<string>) => {
-      if (state.activeSection === action.payload){
-        state.activeSection = 'none';
-      } else {
-        state.activeSection = action.payload;
-      }
-    },
     updatePlays: (state, action: PayloadAction<UpdateAction>) => {
+      let boardFilled = true; //will turn false if any 0s in active board.
       state.activePlays[action.payload.i][action.payload.j] = action.payload.value;
+      for (let i = 0; i < state.activePlays.length; i++){
+        for (let j = 0; j < state.activePlays[i].length; j++){
+          if (state.activePlays[i][j] === 0){
+            boardFilled = false;
+            break;
+          }
+        }
+      }
+      state.boardFilled = boardFilled;
     },
     getElapsed: (state, action: PayloadAction<number>) =>{
       state.currentTime = action.payload;
     },
+    verifySolution: (state) =>{
+      const checkAnswers = new Set();
+      for (let i = 0; i < state.activePlays.length; i++){
+        for (let j = 0; j < state.activePlays[i].length; j++){
+          checkAnswers.add(`row ${i} has ${state.activePlays[i][j]}`);
+          checkAnswers.add(`column ${j} has ${state.activePlays[i][j]}`)
+          checkAnswers.add(`box ${Math.floor(i/2)},${Math.floor(j/2)} has ${state.activePlays[i][j]}`)
+        }
+      }
+      if (checkAnswers.size === 48){
+        console.log('valid answer')
+        state.gameWon = true;
+        state.gameStarted = false;
+      } else {
+        console.log('invalid answer')
+      }
+    }
   },
 })
 
-export const { startGame, updateActive, updatePlays, getElapsed } = boardSlice.actions;
+export const { startGame, updatePlays, getElapsed, verifySolution } = boardSlice.actions;
 export const boardDisplay = (state: RootState) => state.board.board;
 export default boardSlice.reducer;
