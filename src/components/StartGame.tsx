616809137500b188933206992017aa2b9aaeb4c5
Bryan Bart <bebart87@gmail.com>
2022-03-16 21:07:04 -0700

    Game fully playable, needs feedback on win condition -- needs generation of puzzle

--- src/components/StartGame.tsx
+++ src/components/StartGame.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useAppDispatch } from '../app/hooks';
 import { startGame, getElapsed } from '../slices/boardSlice';
 
+
 function StartGame() {
   const dispatch = useAppDispatch();
 
